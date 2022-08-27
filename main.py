613b12b3ce048a89803112acafd198068d7f96af
HarshaanNiles010 <harshaanniles010@gmail.com>
2022-08-26 18:41:02 -0600

    Writting Test Cases

--- main.py
+++ main.py
@@ -6,42 +6,41 @@ import math as mt
 import random
 
 
-def rowPop(arr, coord_X, coord_Y):
-    tempArr = arr.copy()
-    x_Dim = tempArr.shape[0]
-    y_Dim = tempArr.shape[1]
-    for i in range(x_Dim):
-        for j in range(y_Dim):
-            if i == coord_X:
-                tempArr[coord_X][j] = 1
-    return tempArr
+class Board:
+    def __init__(self, arr):
+        self.x_Dim = arr.shape[0]
+        self.y_Dim = arr.shape[1]
+        self.tempArr = arr.copy()
 
+    def rowPop(self, coord_X, coord_Y):
+        for i in range(self.x_Dim):
+            for j in range(self.y_Dim):
+                if i == coord_X:
+                    self.tempArr[coord_X][j] = 1
+        return self.tempArr
 
-def colPop(arr, coord_X, coord_Y):
-    tempArr = arr.copy()
-    x_Dim = tempArr.shape[0]
-    y_Dim = tempArr.shape[1]
-    for i in range(x_Dim):
-        for j in range(y_Dim):
-            if j == coord_Y:
-                tempArr[i][coord_Y] = 1
-    return tempArr
+    def colPop(self, coord_X, coord_Y):
+        for i in range(self.x_Dim):
+            for j in range(self.y_Dim):
+                if j == coord_Y:
+                    self.tempArr[i][coord_Y] = 1
+        return self.tempArr
 
+    def primeDiag(self, coord_X, coord_Y):
+        aux = np.ones(max(coord_X, coord_Y))
+        temp = int(mt.fabs(coord_X - coord_Y))
+        np.fill_diagonal(self.tempArr[:, temp:], aux)
+        return self.tempArr
 
-def primeDiag(arr, coord_X, coord_Y):
-    tempArr = arr.copy()
-    aux = np.ones(max(coord_X, coord_Y))
-    temp = int(mt.fabs(coord_X - coord_Y))
-    np.fill_diagonal(tempArr[:, temp:], aux)
-    return tempArr
-
+    def Print_Dim(self):
+        print(f"The array is: \n{self.tempArr}\n The X-Dimension is: {self.x_Dim}\n The Y-Dimension is: {self.y_Dim}")
 
 # Implementing Secondary Diagonal
 def seconDiag(arr, coord_X, coord_Y):
     tempArr = arr.copy()
     x_Dim = tempArr.shape[0]
     y_Dim = tempArr.shape[1]
-    #coord_X, coord_Y = coord_Y, coord_X
+    # coord_X, coord_Y = coord_Y, coord_X
     aux = np.ones(max(coord_X, coord_Y))
     temp = int(mt.fabs(coord_X - coord_Y))
     np.fill_diagonal(tempArr[::-1, temp:], aux)
@@ -51,8 +50,6 @@ def seconDiag(arr, coord_X, coord_Y):
 arr = np.zeros((5, 5))
 # arr = np.random.rand(5, 5)
 coord_X, coord_Y = 1, 4
-# arr = rowPop(arr,coord_X,coord_Y)
-# arr = colPop(arr,coord_X,coord_Y)
-# arr = primeDiag(arr,coord_X,coord_Y)
-# print(arr)
-seconDiag(arr, coord_X, coord_Y)
+#seconDiag(arr, coord_X, coord_Y)
+B0 = Board(arr)
+B0.Print_Dim()
---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+# This file contains the testing for the heat maps basic operations
+class TestBoard(unittest.TestCase):
+
+    def Test_board_x(self):
+        pass
+
+    def Test_board_y(self):
+        pass
