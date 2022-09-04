304b2e35ae5c9af649816761026985cc210ac89c
HarshaanNiles010 <harshaanniles010@gmail.com>
2022-09-03 20:12:17 -0600

    Implemented getting neighbours

--- main.py
+++ main.py
@@ -41,27 +41,40 @@ class Board:
         np.fill_diagonal(self.tempArr[:, temp:], aux)
         return self.tempArr
 
-
-
-
-
+# implementing a method to get the 8 squares around the coordinate of choice
+def get_neighbours(arr,coord_X,coord_Y):
+    tempArr = arr.copy()
+    #tempArr = tempArr.tolist()
+    result = []
+    for rowAdd in range(-1,2):
+        newRow = coord_X + rowAdd
+        if newRow >= 0 and newRow <= tempArr.shape[0] -1:
+            for colAdd in range(-1,2):
+                newCol = coord_Y + colAdd
+                if newCol >= 0 and newCol <= tempArr.shape[1] - 1:
+                    if newCol == coord_Y and newRow == coord_X:
+                        continue
+                    result.append((newRow,newCol))
+    return result
 
 # Implementing Secondary Diagonal
 def seconDiag(arr, coord_X, coord_Y):
     tempArr = arr.copy()
     x_Dim = tempArr.shape[0]
     y_Dim = tempArr.shape[1]
-    # coord_X, coord_Y = coord_Y, coord_X
-    aux = np.ones(max(coord_X, coord_Y))
-    temp = int(mt.fabs(coord_X - coord_Y))
-    np.fill_diagonal(tempArr[::-1, temp:], aux)
-    print(tempArr)
+    temp = []
+    for i in range(x_Dim):
+        for j in range(y_Dim):
+            if i+1 == j-1:
+                temp.append((i,j))
 
 
 if __name__ == '__main__':
     arr = np.zeros((5, 5))
-    # arr = np.random.rand(5, 5)
+    #arr = np.random.rand(5, 5)
+    print(arr)
     coord_X, coord_Y = 1, 4
-    # seconDiag(arr, coord_X, coord_Y)
-    B0 = Board(arr)
+    #seconDiag(arr, coord_X, coord_Y)
+    #B0 = Board(arr)
+    get_neighbours(arr,coord_X,coord_Y)
 
