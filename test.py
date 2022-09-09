b2bc0307caeaf2c561e58fec35957fc2912a9405
HarshaanNiles010 <harshaanniles010@gmail.com>
2022-09-09 00:04:47 -0600

    writing test cases for winning conditions

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from main import Board
+from main import Board,Player
 
 
 # This file contains the testing for the heat maps basic operations
@@ -17,6 +17,20 @@ class TestBoard(unittest.TestCase):
         arr = np.zeros((5, 5))
         B0 = Board(arr)
         self.assertEqual(B0.get_y_dim(), 5)
+
+    def test_check_Row_Win(self):
+        arr = np.zeros((5,5))
+        P0 = Player(arr)
+        pass
+
+    def test_check_Col_Win(self):
+        pass
+
+    def test_check_Pdi_Win(self):
+        pass
+
+    def test_check_Sdi_Win(self):
+        pass
 
 
 if __name__ == '__main__':
