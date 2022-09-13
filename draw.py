d6a46fc9649ec32b27655f75fc0daeecbd7a1239
HarshaanNiles010 <harshaanniles010@gmail.com>
2022-09-12 23:29:43 -0600

    draw

--- draw.py
+++ draw.py
@@ -1,95 +1,17 @@
-import pygame
-from pygame.locals import*
-import time
-import math
-from math import *
-import OpenGL
-from OpenGL.GL import *
-from OpenGL.GLU import *
-import random
+import matplotlib.pyplot as plt
+import numpy
 
-x = 0.01
-y = 0
-z = 0
+# Set up grid and test data
+nx, ny = 256, 1024
+x = range(nx)
+y = range(ny)
 
-angle = 0.01
-display = (1250,950)
+data = numpy.random.random((nx, ny))
 
-counter = 0
-def lorenz_attractor(x,y,z,counter):
+hf = plt.figure()
+ha = hf.add_subplot(111, projection='3d')
 
-    #parameters of lorenz attractor
-    # let a be sigma
-    # let b be rho
-    # let c be beta
-    dt = 0.01
-    a = 10.0
-    b = 28
-    c = (8/3)
-    dx = 0
-    dy = 0
-    dz = 0
-    while(counter !=1000):
-        glColor3d(1, 1, 1)
-        #formula for the three differential equations
-        dx = (a*(y - x)) * dt
-        dy = (x*(b - z) - y) * dt
-        dz = (x*y - c*z) * dt
-        
-        glVertex3d(x,y,z)
-        
-        x = x + dx
-        y = y + dy
-        z = z + dz
-        counter+=1
-    
-def main(angle):
-    #initialize pygame
-    pygame.init()
-    
-    #set the display for the OpenGL
-    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-    gluPerspective(45,(display[0]/display[1]), 0.1, 1500)
+X, Y = numpy.meshgrid(x, y)  # `plot_surface` expects `x` and `y` data to be 2D
+ha.plot_surface(X, Y, data)
 
-    glVertex3d(0, 0, -1)
-    glRotatef(0,0,0,0)
-
-    glTranslatef(0,0,0)
-    
-    clock = pygame.time.Clock()
-    
-    while (True) :
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-        
-        clock.tick(69)
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        
-        #time.sleep(0.1)
-        #pygame.time.wait(10)
-        
-        #zoom out, translate points and rotate
-        if (angle<1):
-            glRotatef(angle,-0.5,-angle,-5)
-            glTranslatef(0,0,-0.5)
-        
-        #zoom in, translate points and rotate
-        if (angle>1):
-            glRotatef(0.9,5,1,-1)
-            glTranslatef(0,0,0.01)
-            
-        glEnable(GL_POINT_SMOOTH)
-        glPointSize(1)
-
-        glBegin(GL_POINTS)
-    
-        lorenz_attractor(x, y, z, counter)
-            
-        glEnd()
-        
-        pygame.display.flip()
-        angle+=0.005
-        
-main(angle)
+plt.show()
